f95267e5d582d1a0d2d9258048305998c5f09b35
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:09:52 +0700

    #9 - Add ApiExceptionHandler

---
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/exception/ApiExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.hendisantika.ecommerce.springbootecommerce.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-26
+ * Time: 06:08
+ */
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+}
