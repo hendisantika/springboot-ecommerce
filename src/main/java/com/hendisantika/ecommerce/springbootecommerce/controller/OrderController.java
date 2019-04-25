37e20a778fe6d277b21517dabd8bb37ec5e3c137
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:21:39 +0700

    #27 - Add create Order method in Order Controller

--- src/main/java/com/hendisantika/ecommerce/springbootecommerce/controller/OrderController.java
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/controller/OrderController.java
@@ -1,16 +1,25 @@
 package com.hendisantika.ecommerce.springbootecommerce.controller;
 
+import com.hendisantika.ecommerce.springbootecommerce.dto.OrderProductDto;
+import com.hendisantika.ecommerce.springbootecommerce.exception.ResourceNotFoundException;
 import com.hendisantika.ecommerce.springbootecommerce.model.Order;
+import com.hendisantika.ecommerce.springbootecommerce.model.OrderProduct;
+import com.hendisantika.ecommerce.springbootecommerce.model.OrderStatus;
 import com.hendisantika.ecommerce.springbootecommerce.service.OrderProductService;
 import com.hendisantika.ecommerce.springbootecommerce.service.OrderService;
 import com.hendisantika.ecommerce.springbootecommerce.service.ProductService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -39,5 +48,61 @@ public class OrderController {
     @ResponseStatus(HttpStatus.OK)
     public @NotNull Iterable<Order> listAllOrders() {
         return this.orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> create(@RequestBody OrderForm form) {
+        List<OrderProductDto> formDtos = form.getProductOrders();
+        validateProductsExistence(formDtos);
+        Order order = new Order();
+        order.setStatus(OrderStatus.PAID.name());
+        order = this.orderService.create(order);
+
+        List<OrderProduct> orderProducts = new ArrayList<>();
+        for (OrderProductDto dto : formDtos) {
+            orderProducts.add(orderProductService.create(new OrderProduct(order, productService.getProduct(dto
+                    .getProduct()
+                    .getId()), dto.getQuantity())));
+        }
+
+        order.setOrderProducts(orderProducts);
+
+        this.orderService.update(order);
+
+        String uri = ServletUriComponentsBuilder
+                .fromCurrentServletMapping()
+                .path("/orders/{id}")
+                .buildAndExpand(order.getId())
+                .toString();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", uri);
+
+        return new ResponseEntity<>(order, headers, HttpStatus.CREATED);
+    }
+
+    private void validateProductsExistence(List<OrderProductDto> orderProducts) {
+        List<OrderProductDto> list = orderProducts
+                .stream()
+                .filter(op -> Objects.isNull(productService.getProduct(op
+                        .getProduct()
+                        .getId())))
+                .collect(Collectors.toList());
+
+        if (!CollectionUtils.isEmpty(list)) {
+            new ResourceNotFoundException("Product not found");
+        }
+    }
+
+    public static class OrderForm {
+
+        private List<OrderProductDto> productOrders;
+
+        public List<OrderProductDto> getProductOrders() {
+            return productOrders;
+        }
+
+        public void setProductOrders(List<OrderProductDto> productOrders) {
+            this.productOrders = productOrders;
+        }
     }
 }
