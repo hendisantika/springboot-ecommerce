50f309a6ee444ad2afb37c28c881e3ea793aed47
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:07:53 +0700

    #7 - Add OrderProduct Model

---
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/model/OrderProduct.java
@@ -0,0 +1,92 @@
+package com.hendisantika.ecommerce.springbootecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Transient;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-26
+ * Time: 06:06
+ */
+@Entity
+public class OrderProduct {
+
+    @EmbeddedId
+    @JsonIgnore
+    private OrderProductPK pk;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public OrderProduct() {
+        super();
+    }
+
+    public OrderProduct(Order order, Product product, Integer quantity) {
+        pk = new OrderProductPK();
+        pk.setOrder(order);
+        pk.setProduct(product);
+        this.quantity = quantity;
+    }
+
+    @Transient
+    public Product getProduct() {
+        return this.pk.getProduct();
+    }
+
+    @Transient
+    public Double getTotalPrice() {
+        return getProduct().getPrice() * getQuantity();
+    }
+
+    public OrderProductPK getPk() {
+        return pk;
+    }
+
+    public void setPk(OrderProductPK pk) {
+        this.pk = pk;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((pk == null) ? 0 : pk.hashCode());
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        OrderProduct other = (OrderProduct) obj;
+        if (pk == null) {
+            return other.pk == null;
+        } else return pk.equals(other.pk);
+
+    }
+}
