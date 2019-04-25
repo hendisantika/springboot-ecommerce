f8d13f1b89082f0429c3dec36b72ecfc6dc03883
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:00:21 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/SpringbootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.ecommerce.springbootecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/ecommerce/springbootecommerce/SpringbootEcommerceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.ecommerce.springbootecommerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootEcommerceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
