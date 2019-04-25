c3bb16f9bc69cc42d8d45c2e605da48115999609
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:13:14 +0700

    #16 - Add ProductRepository

---
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.ecommerce.springbootecommerce.repository;
+
+import com.hendisantika.ecommerce.springbootecommerce.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-26
+ * Time: 06:13
+ */
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
