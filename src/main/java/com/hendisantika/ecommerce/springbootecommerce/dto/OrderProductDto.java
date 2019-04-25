6529948b78a3a02fe76f421e0bd3241749af55e9
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:08:28 +0700

    #8 - Add OrderProduct DTO

---
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/dto/OrderProductDto.java
@@ -0,0 +1,33 @@
+package com.hendisantika.ecommerce.springbootecommerce.dto;
+
+import com.hendisantika.ecommerce.springbootecommerce.model.Product;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-26
+ * Time: 06:08
+ */
+public class OrderProductDto {
+    private Product product;
+    private Integer quantity;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
