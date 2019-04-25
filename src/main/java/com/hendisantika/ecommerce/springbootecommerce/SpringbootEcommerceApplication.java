8524ba62a0f63d86aa3a36cf257fea812fdcb4b6
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:17:19 +0700

    #23 - Add Initialize data in Main Class

--- src/main/java/com/hendisantika/ecommerce/springbootecommerce/SpringbootEcommerceApplication.java
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/SpringbootEcommerceApplication.java
@@ -1,13 +1,30 @@
 package com.hendisantika.ecommerce.springbootecommerce;
 
+import com.hendisantika.ecommerce.springbootecommerce.model.Product;
+import com.hendisantika.ecommerce.springbootecommerce.service.ProductService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootEcommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootEcommerceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(ProductService productService) {
+        return args -> {
+            productService.save(new Product(1L, "TV Set", 300.00, "http://placehold.it/200x100"));
+            productService.save(new Product(2L, "Game Console", 200.00, "http://placehold.it/200x100"));
+            productService.save(new Product(3L, "Sofa", 100.00, "http://placehold.it/200x100"));
+            productService.save(new Product(4L, "Icecream", 5.00, "http://placehold.it/200x100"));
+            productService.save(new Product(5L, "Beer", 3.00, "http://placehold.it/200x100"));
+            productService.save(new Product(6L, "Phone", 500.00, "http://placehold.it/200x100"));
+            productService.save(new Product(7L, "Watch", 30.00, "http://placehold.it/200x100"));
+        };
     }
 
 }
