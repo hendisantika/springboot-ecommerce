20d343cc725297d6652d511db9fb3fca8865418d
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:25:23 +0700

    #32 - Add contextLoads Test Case in EcommerceApplicationIntegrationTest

--- src/test/java/com/hendisantika/ecommerce/springbootecommerce/EcommerceApplicationIntegrationTest.java
+++ src/test/java/com/hendisantika/ecommerce/springbootecommerce/EcommerceApplicationIntegrationTest.java
@@ -1,7 +1,14 @@
 package com.hendisantika.ecommerce.springbootecommerce;
 
+import com.hendisantika.ecommerce.springbootecommerce.controller.OrderController;
+import com.hendisantika.ecommerce.springbootecommerce.controller.ProductController;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -16,4 +23,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {SpringbootEcommerceApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EcommerceApplicationIntegrationTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private ProductController productController;
+
+    @Autowired
+    private OrderController orderController;
+
+    @Test
+    public void contextLoads() {
+        Assertions
+                .assertThat(productController)
+                .isNotNull();
+        Assertions
+                .assertThat(orderController)
+                .isNotNull();
+    }
 }
