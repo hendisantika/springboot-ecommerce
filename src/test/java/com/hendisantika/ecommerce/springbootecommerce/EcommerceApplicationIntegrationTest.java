bda4cd806019d9f1d08da7e07e5caa218c8dbc4f
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:27:20 +0700

    #33 - Add givenGetProductsApiCall_whenProductListRetrieved_thenSizeMatchAndListContainsProductNames Test Case in EcommerceApplicationIntegrationTest

--- src/test/java/com/hendisantika/ecommerce/springbootecommerce/EcommerceApplicationIntegrationTest.java
+++ src/test/java/com/hendisantika/ecommerce/springbootecommerce/EcommerceApplicationIntegrationTest.java
@@ -2,6 +2,7 @@ package com.hendisantika.ecommerce.springbootecommerce;
 
 import com.hendisantika.ecommerce.springbootecommerce.controller.OrderController;
 import com.hendisantika.ecommerce.springbootecommerce.controller.ProductController;
+import com.hendisantika.ecommerce.springbootecommerce.model.Product;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -43,5 +52,23 @@ public class EcommerceApplicationIntegrationTest {
         Assertions
                 .assertThat(orderController)
                 .isNotNull();
+    }
+
+    @Test
+    public void givenGetProductsApiCall_whenProductListRetrieved_thenSizeMatchAndListContainsProductNames() {
+        ResponseEntity<Iterable<Product>> responseEntity = restTemplate.exchange("http://localhost:" + port + "/api/products", HttpMethod.GET, null, new ParameterizedTypeReference<Iterable<Product>>() {
+        });
+        Iterable<Product> products = responseEntity.getBody();
+        Assertions
+                .assertThat(products)
+                .hasSize(7);
+
+        assertThat(products, hasItem(hasProperty("name", is("TV Set"))));
+        assertThat(products, hasItem(hasProperty("name", is("Game Console"))));
+        assertThat(products, hasItem(hasProperty("name", is("Sofa"))));
+        assertThat(products, hasItem(hasProperty("name", is("Icecream"))));
+        assertThat(products, hasItem(hasProperty("name", is("Beer"))));
+        assertThat(products, hasItem(hasProperty("name", is("Phone"))));
+        assertThat(products, hasItem(hasProperty("name", is("Watch"))));
     }
 }
