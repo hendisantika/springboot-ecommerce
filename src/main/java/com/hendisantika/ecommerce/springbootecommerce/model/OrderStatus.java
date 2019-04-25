479863b82cf9319fbbd8ae64779d8cac39e8757c
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:06:08 +0700

    #5 - Add Order Status Model

---
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/model/OrderStatus.java
@@ -0,0 +1,17 @@
+package com.hendisantika.ecommerce.springbootecommerce.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-26
+ * Time: 06:05
+ */
+public enum OrderStatus {
+    PAID,
+    UNPAID,
+    FAILED,
+    CANCEL
+}
