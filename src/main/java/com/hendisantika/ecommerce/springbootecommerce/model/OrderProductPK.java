819e4a626cfbf9a4467fbb2c27ecd2b275696dee
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:07:17 +0700

    #6 - Add OrderProductPK Model

---
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/model/OrderProductPK.java
@@ -0,0 +1,96 @@
+package com.hendisantika.ecommerce.springbootecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-26
+ * Time: 06:06
+ */
+@Embeddable
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "order")
+public class OrderProductPK implements Serializable {
+
+    private static final long serialVersionUID = 476151177562655457L;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + ((order.getId() == null)
+                ? 0
+                : order
+                .getId()
+                .hashCode());
+        result = prime * result + ((product.getId() == null)
+                ? 0
+                : product
+                .getId()
+                .hashCode());
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        OrderProductPK other = (OrderProductPK) obj;
+        if (order == null) {
+            if (other.order != null) {
+                return false;
+            }
+        } else if (!order.equals(other.order)) {
+            return false;
+        }
+
+        if (product == null) {
+            return other.product == null;
+        } else return product.equals(other.product);
+
+    }
+}
+
