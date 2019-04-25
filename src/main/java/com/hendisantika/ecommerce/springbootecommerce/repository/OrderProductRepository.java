750224bdaefb79e9c84131cb918c1be8c47d2543
hendisantika <hendisantika@yahoo.co.id>
2019-04-26 06:12:27 +0700

    #14 - Add OrderProductRepository

---
+++ src/main/java/com/hendisantika/ecommerce/springbootecommerce/repository/OrderProductRepository.java
@@ -0,0 +1,17 @@
+package com.hendisantika.ecommerce.springbootecommerce.repository;
+
+import com.hendisantika.ecommerce.springbootecommerce.model.OrderProduct;
+import com.hendisantika.ecommerce.springbootecommerce.model.OrderProductPK;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-26
+ * Time: 06:12
+ */
+public interface OrderProductRepository extends CrudRepository<OrderProduct, OrderProductPK> {
+}
