8a94b81f42fdc5db7ad42ea4c9b23c47a2f8b9c4
hendisantika <hendisantika@yahoo.co.id>
2019-04-27 05:23:54 +0700

    #34 - Add givenPostOrder_whenBodyRequestMatcherJson_thenResponseContainsEqualObjectProperties Test Case in EcommerceApplicationIntegrationTest

--- src/test/java/com/hendisantika/ecommerce/springbootecommerce/EcommerceApplicationIntegrationTest.java
+++ src/test/java/com/hendisantika/ecommerce/springbootecommerce/EcommerceApplicationIntegrationTest.java
@@ -2,6 +2,8 @@ package com.hendisantika.ecommerce.springbootecommerce;
 
 import com.hendisantika.ecommerce.springbootecommerce.controller.OrderController;
 import com.hendisantika.ecommerce.springbootecommerce.controller.ProductController;
+import com.hendisantika.ecommerce.springbootecommerce.dto.OrderProductDto;
+import com.hendisantika.ecommerce.springbootecommerce.model.Order;
 import com.hendisantika.ecommerce.springbootecommerce.model.Product;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -12,8 +14,11 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
 
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
@@ -70,5 +75,27 @@ public class EcommerceApplicationIntegrationTest {
         assertThat(products, hasItem(hasProperty("name", is("Beer"))));
         assertThat(products, hasItem(hasProperty("name", is("Phone"))));
         assertThat(products, hasItem(hasProperty("name", is("Watch"))));
+    }
+
+    @Test
+    public void givenPostOrder_whenBodyRequestMatcherJson_thenResponseContainsEqualObjectProperties() {
+        final ResponseEntity<Order> postResponse = restTemplate.postForEntity("http://localhost:" + port + "/api/orders", prepareOrderForm(), Order.class);
+        Order order = postResponse.getBody();
+        Assertions
+                .assertThat(postResponse.getStatusCode())
+                .isEqualByComparingTo(HttpStatus.CREATED);
+
+        assertThat(order, hasProperty("status", is("PAID")));
+        assertThat(order.getOrderProducts(), hasItem(hasProperty("quantity", is(2))));
+    }
+
+    private OrderController.OrderForm prepareOrderForm() {
+        OrderController.OrderForm orderForm = new OrderController.OrderForm();
+        OrderProductDto productDto = new OrderProductDto();
+        productDto.setProduct(new Product(1L, "TV Set", 300.00, "http://placehold.it/200x100"));
+        productDto.setQuantity(2);
+        orderForm.setProductOrders(Collections.singletonList(productDto));
+
+        return orderForm;
     }
 }
