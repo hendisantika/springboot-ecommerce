afe509ebbe4862c6fe8456990e2a63cf93eecb4b
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-14 05:26:42 +0700

    #51 - ng g c /ecommerce/products - Frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {EcommerceComponent} from './ecommerce/ecommerce.component';
+import {ProductsComponent} from './ecommerce/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EcommerceComponent
+    EcommerceComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
