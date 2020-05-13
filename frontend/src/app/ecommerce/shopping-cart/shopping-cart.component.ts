300ca10ef026a7e5deea4e37b47c5773a34cf8da
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-14 06:21:13 +0700

    #67 - In the ShoppingCart component, we'll also inject the service. We'll use it to subscribe to the changes in the ProductsComponent (to notice when the product is selected to be put in the shopping cart) and then update the content of the cart and recalculate the total cost accordingly - Frontend

--- frontend/src/app/ecommerce/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/ecommerce/shopping-cart/shopping-cart.component.ts
@@ -1,16 +1,53 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {ProductOrders} from "../models/product-orders.model";
+import {Subscription} from "rxjs";
+import {EcommerceService} from "../services/ecommerce.service";
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent implements OnInit, OnDestroy {
+  orderFinished: boolean;
+  orders: ProductOrders;
+  total: number;
+  sub: Subscription;
 
-  constructor() {
+  @Output() onOrderFinished: EventEmitter<boolean>;
+
+  constructor(private ecommerceService: EcommerceService) {
+    this.total = 0;
+    this.orderFinished = false;
+    this.onOrderFinished = new EventEmitter<boolean>();
   }
 
   ngOnInit() {
+    this.orders = new ProductOrders();
+    this.loadCart();
+    this.loadTotal();
   }
 
+  loadTotal() {
+    this.sub = this.ecommerceService.OrdersChanged.subscribe(() => {
+      this.total = this.calculateTotal(this.orders.productOrders);
+    });
+  }
+
+  loadCart() {
+    this.sub = this.ecommerceService.ProductOrderChanged.subscribe(() => {
+      let productOrder = this.ecommerceService.SelectedProductOrder;
+      if (productOrder) {
+        this.orders.productOrders.push(new ProductOrder(
+          productOrder.product, productOrder.quantity));
+      }
+      this.ecommerceService.ProductOrders = this.orders;
+      this.orders = this.ecommerceService.ProductOrders;
+      this.total = this.calculateTotal(this.orders.productOrders);
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
