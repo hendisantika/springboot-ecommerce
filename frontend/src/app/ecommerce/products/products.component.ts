378aeac88ae8c349fc76b8af4477f03ce5df4d70
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-14 06:10:14 +0700

    #63 - Add loadProducts & loadOrders - Frontend

--- frontend/src/app/ecommerce/products/products.component.ts
+++ frontend/src/app/ecommerce/products/products.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {ProductOrder} from "../models/product-order.model";
+import {EcommerceService} from "../services/ecommerce.service";
+import {Subscription} from "rxjs";
+import {ProductOrders} from "../models/product-orders.model";
+import {Product} from "../models/product.model";
 
 @Component({
   selector: 'app-products',
@@ -7,10 +12,39 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() {
+  productOrders: ProductOrder[] = [];
+  products: Product[] = [];
+  selectedProductOrder: ProductOrder;
+  sub: Subscription;
+  productSelected: boolean = false;
+  private shoppingCartOrders: ProductOrders;
+
+  constructor(private ecommerceService: EcommerceService) {
   }
 
   ngOnInit() {
+    this.productOrders = [];
+    this.loadProducts();
+    this.loadOrders();
+  }
+
+  loadProducts() {
+    this.ecommerceService.getAllProducts()
+      .subscribe(
+        (products: any[]) => {
+          this.products = products;
+          this.products.forEach(product => {
+            this.productOrders.push(new ProductOrder(product, 0));
+          })
+        },
+        (error) => console.log(error)
+      );
+  }
+
+  loadOrders() {
+    this.sub = this.ecommerceService.OrdersChanged.subscribe(() => {
+      this.shoppingCartOrders = this.ecommerceService.ProductOrders;
+    });
   }
 
 }
