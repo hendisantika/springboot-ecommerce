e9bb8533d252a7b72957a264c2b9deab743b64a7
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-14 05:24:37 +0700

    #50 - Generate ecommerce component - Frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {EcommerceComponent} from './ecommerce/ecommerce.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EcommerceComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/ecommerce/ecommerce.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce',
+  templateUrl: './ecommerce.component.html',
+  styleUrls: ['./ecommerce.component.css']
+})
+export class EcommerceComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
