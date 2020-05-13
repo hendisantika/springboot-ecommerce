745503db22f6018390e4079fe529aaf2e6dfb8e8
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-14 05:27:27 +0700

    #53 - ng g c /ecommerce/shopping-cart - Frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import {AppComponent} from './app.component';
 import {EcommerceComponent} from './ecommerce/ecommerce.component';
 import {ProductsComponent} from './ecommerce/products/products.component';
 import {OrdersComponent} from './ecommerce/orders/orders.component';
+import {ShoppingCartComponent} from './ecommerce/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EcommerceComponent,
     ProductsComponent,
-    OrdersComponent
+    OrdersComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
